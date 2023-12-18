11c2e27bc18ce6269c56d7fcf648b86931045076
jenni4j <jenni4j@gmail.com>
2023-12-18 15:04:29 -0800

    about page completed

--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from "./pages/Home";
 import Navbar from "./components/NavBar/Navbar";
 import Intro from "./components/Intro/Intro";
+import About from "./components/About/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Intro />
+      <About />
       </div>
   );
 }
