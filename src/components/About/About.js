36943e1b2d9054dc35c58666c81acb3a68ca114a
jenni4j <jenni4j@gmail.com>
2023-12-24 12:22:59 -0800

    added github icon and about section details

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,19 @@ function About() {
         <section id='about'>
             <span className="aboutTitle"></span>
             <span className="aboutContent">
-            Hi, I’m Jenn - welcome to my portfolio! I’m in the process of completing a masters degree in computer science, with a focus on machine learning, 
-            so the projects you will find here are often inspired from things I am currently learning. 
-            I love thinking about how to apply data science and machine learning to pressing global issues or ways to improve human life.
-            Feel free to reach out if collaborating on projects would be interesting to you!</span>
+            <p>Hi, I’m Jenn - welcome to my portfolio! I’m currently based in Vancouver, Canada and in the process of completing a masters degree 
+            remotely at Georgia Tech in computer science, with a focus on machine learning.</p>
+            
+            <p>Prior to pursuing a graduate degree, I studied business at the University of British Columbia and gained a few years of experience in the finance industry. 
+                Highlights include working on the trading floor at Bank of America and as a data analyst on quantitative investing teams at the Canada Pension Plan Investment 
+                Board and Connor, Clark & Lunn Investment Management. </p> 
+            
+            <p>I love thinking about how to apply technology to pressing global issues or ways to improve human life, which is why the projects you will find here 
+                are often inspired by this, as well as tools I am currently learning. Some themes that will often spark my interest are climate change and the environment, 
+                economic inequality, and healthcare. </p>
+            
+            When I’m not in front of a computer screen, you can usually find me in the mountains on bikes or skis, or playing Chopin on the piano. 
+            Feel free to reach out if collaborating on projects would be of interest to you!</span>
         </section>        
     );
   }
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,6 +4,7 @@ import FloodImage from "./flood-image.jpeg";
 import StockImage from "./stock-chart.jpeg";
 import DogImage from "./dog-breed.jpeg";
 import MLImage from "./ml-safety.jpeg";
+import GitHubIcon from "./github-logo.png"
 
 function Portfolio() {
 
@@ -18,7 +19,10 @@ function Portfolio() {
                 <div className="portfolioItem" onClick={() => handleItemClick('https://github.com/jenni4j/Flood-Detection')}>
                     <img src={FloodImage} alt="" className="portfolioItemImg" />  
                     <div className="portfolioItemText">
-                        <h2>Identifying Flood Water from Satellite Imagery</h2>
+                        <div className="portfolioItemHeader">
+                            <h2>Identifying Flood Water from Satellite Imagery</h2>
+                            <img src={GitHubIcon} alt="" className="portfolioItemIcon" />
+                        </div>
                         <p>Final project for a deep learning course at Georgia Tech. 
                             Various fully convolutional network architectures were implemented and trained to 
                             create segmentation maps of flooded areas. A vision transformer pretrained on generic satellite data 
@@ -28,7 +32,10 @@ function Portfolio() {
                 <div className="portfolioItem" onClick={() => handleItemClick('https://github.com/jenni4j/Kaggle-Dog-Breed-Identification/')}>
                     <img src={DogImage} alt="" className="portfolioItemImg" />  
                     <div className="portfolioItemText">
-                        <h2>Classifying Dog Breeds</h2>
+                        <div className="portfolioItemHeader">
+                            <h2>Classifying Dog Breeds</h2>
+                            <img src={GitHubIcon} alt="" className="portfolioItemIcon" />
+                        </div>
                         <p>Used transfer learning with InceptionV3 as the base pretrained model to classify 120 different breeds of dogs as part of
                             a Kaggle competition.</p>
                     </div>
@@ -36,7 +43,10 @@ function Portfolio() {
                 <div className="portfolioItem" onClick={() => handleItemClick('https://github.com/jenni4j/Stock-Sentiment')}>
                     <img src={StockImage} alt="" className="portfolioItemImg" />  
                     <div className="portfolioItemText">
-                        <h2>Stock Sentiment Tool</h2>
+                        <div className="portfolioItemHeader">
+                            <h2>Stock Sentiment Tool</h2>
+                            <img src={GitHubIcon} alt="" className="portfolioItemIcon" />
+                        </div>
                         <p>Developed a tool that allows a user to perform sentiment analysis on news headlines for a specific stock ticker using the 
                             NLTK natural language library.
                         </p>
@@ -45,7 +55,10 @@ function Portfolio() {
                 <div className="portfolioItem" onClick={() => handleItemClick('https://github.com/jenni4j/CAIS-Intro-to-ML-Safety')}>
                     <img src={MLImage} alt="" className="portfolioItemImg" />  
                     <div className="portfolioItemText">
-                        <h2>Intro to Machine Learning Safety</h2>
+                        <div className="portfolioItemHeader">
+                            <h2>Intro to Machine Learning Safety</h2>
+                            <img src={GitHubIcon} alt="" className="portfolioItemIcon" />
+                        </div>
                         <p>A repository with completed assignments from an Introduction to Machine Learning Safety course hosted by the Center for AI Safety.</p>
                     </div>
                 </div>
